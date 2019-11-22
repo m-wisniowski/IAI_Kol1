6fc45c5f8923fa03e4931b7c7d50de17c2a34c7a
m-wisniowski <56130530+m-wisniowski@users.noreply.github.com>
2019-11-22 09:19:27 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,11 @@
+let nav='<ul> \n' +
+    '<font face="Arial Black" size="3"> \n' +
+    ' <h1>Nawigacja</h1>\n' +
+    '<ul> \n' +
+    '<li class="nav-item"><a class="nav-link" target="_blank" href="index.html">Przycisk</a></li> \n' +
+    '<li class="nav-item"><a class="nav-link" target="_blank" href="kontakt.html">Kontakt</a></li> \n' +
+    '</font> \n' +
+    '</ul>'
+
+let elem=document.querySelector(".navbar");
+elem.innerHTML=nav;
---
+++ script.js
@@ -0,0 +1,4 @@
+function zmien(){
+    var kwadrat = document.getElementById("akcja");
+    akcja.style.backgroundColor = "red";
+}
